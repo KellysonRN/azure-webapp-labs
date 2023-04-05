37c63a9022749e337550b8641a5b757ef81a80ae
Kellyson Santos <kellyson.victor@gmail.com>
2023-04-04 21:13:44 -0300

    Setup minimal API with xUnit

--- src/DotNetProject.Api/Program.cs
+++ src/DotNetProject.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var pingPongSecret = builder.Configuration["PING"];
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,17 +10,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MapGet("/", () => pingPongSecret);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
